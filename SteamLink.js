b7b6576721b8a3ce71d5ac71044a305664142606
Maxi Martínez <maxinm@live.com>
2023-07-07 20:58:01 -0300

    Update SteamLink.js

--- SteamLink.js
+++ SteamLink.js
@@ -6,7 +6,29 @@
  * @url 
  *
  * @help
+ * 
+ * @command screenName
+ * @text Get Screen Name
+ * @desc Get the Steam screen name
+ * 
+ * @arg variableId
+ * @type variable
+ * @text Variable
+ * @desc The variable where the screen name will be stored
+ * 
+ * @command uiLanguage
+ * @text Get UI Language
+ * @desc Get the Steam UI language
+ * 
+ * @arg variableId
+ * @type variable
+ * @text Variable
+ * @desc The variable where the UI language will be stored
+ * 
+ * // Agrega aquí más comandos para los otros comandos
 */
+
+
 (function () {
 'use strict';
 
@@ -134,3 +156,543 @@ class e extends SteamLinkPatcher {
   }) {
       return !(!1 !== t || !r) || (!(!0 !== t || !e) || !(void 0 === t || !s))
   }
+  static runEvent(t, {
+      abortOnTrue: e = !1,
+      abortOnFalse: r = !1,
+      returnOnValue: s = !1
+  } = {}, ...a) {
+      if (!this.eventListeners.has(t)) return;
+      const n = this.eventListeners.get(t);
+      for (const t of n) {
+          if ("number" == typeof t) {
+              this.runCommonEvent(t);
+              continue
+          }
+          if ("function" != typeof t) {
+              console.error("SteamLinkEngine: Invalid callback type:"), console.log(t);
+              continue
+          }
+          const n = t(...a);
+          if (this.shouldReturnCallbackResult(n, {
+                  abortOnTrue: e,
+                  abortOnFalse: r,
+                  returnOnValue: s
+              })) return n
+      }
+  }
+  static runCommonEvent(t) {
+      const e = globalThis.$dataCommonEvents[t];
+      if (!e) return;
+      const r = new Game_Interpreter(1);
+      if (r.setup(e.list, 0), !this._interpreters) {
+          this._interpreters = new Set;
+          const t = SceneManager.updateMain;
+          SceneManager.updateMain = () => {
+              t.call(SceneManager), this.update();
+          };
+      }
+      this._interpreters.add(r);
+  }
+  static update() {
+      if (this._interpreters)
+          for (const t of this._interpreters) t.update(), t.isRunning() || this._interpreters.delete(t);
+  }
+  static getPluginFileName() {
+      return this.fileName ?? this.pluginName
+  }
+  static isTrue(e) {
+      return "string" != typeof e ? Boolean(e) : t.includes(e.toUpperCase())
+  }
+  static isFalse(t) {
+      return !this.isTrue(t)
+  }
+  static getIntParam({
+      value: t,
+      defaultValue: e
+  }) {
+      try {
+          const r = parseInt(t);
+          return isNaN(r) ? e : r
+      } catch (r) {
+          return "" !== t && console.error(`SteamLink Engine plugin ${this.pluginName}: Param is expected to be an integer number, but the received value was '${t}'.`), e
+      }
+  }
+  static getFloatParam({
+      value: t,
+      defaultValue: e
+  }) {
+      try {
+          const r = parseFloat(t.replace(",", "."));
+          return isNaN(r) ? e : r
+      } catch (r) {
+          return "" !== t && console.error(`SteamLink Engine plugin ${this.pluginName}: Param is expected to be a number, but the received value was '${t}'.`), e
+      }
+  }
+  static getIntListParam({
+      value: t
+  }) {
+      return this.parseArray((t ?? "").trim(), (t => {
+          try {
+              return parseInt(t.trim())
+          } catch (e) {
+              return "" !== t && console.error(`SteamLink Engine plugin ${this.pluginName}: Param is expected to be a list of integer numbers, but one of the items was '${t}'.`), 0
+          }
+      }))
+  }
+  static parseStructArrayParam({
+      data: t,
+      type: e
+  }) {
+      const r = [];
+      for (const s of t) {
+          const t = this.parseStructParam({
+              value: s,
+              defaultValue: "",
+              type: e
+          });
+          t && r.push(t);
+      }
+      return r
+  }
+  static getFloatListParam({
+      value: t
+  }) {
+      return this.parseArray((t || "").trim(), (t => {
+          try {
+              return parseFloat(t.trim())
+          } catch (e) {
+              return "" !== t && console.error(`SteamLink Engine plugin ${this.pluginName}: Param ${name} is expected to be a list of numbers, but one of the items was '${t}'.`), 0
+          }
+      }))
+  }
+  static getParam({
+      value: t,
+      defaultValue: e,
+      type: r
+  }) {
+      if (r.endsWith("[]")) return this.parseArrayParam({
+          value: t,
+          type: r
+      });
+      if (r.startsWith("struct<")) return this.parseStructParam({
+          value: t,
+          defaultValue: e,
+          type: r
+      });
+      if (void 0 === t) return e;
+      switch (r) {
+          case "int":
+              return this.getIntParam({
+                  value: t,
+                  defaultValue: e
+              });
+          case "float":
+              return this.getFloatParam({
+                  value: t,
+                  defaultValue: e
+              });
+          case "boolean":
+              return "boolean" == typeof t ? t : this.isTrue(String(t).trim());
+          default:
+              return t
+      }
+  }
+  static getPluginParam(t) {
+      return this.params.get(t)
+  }
+  static defaultValueForType(t) {
+      switch (t) {
+          case "int":
+              return 0;
+          case "boolean":
+              return !1
+      }
+      return ""
+  }
+  static parseParam(t, e, r) {
+      let s = e[t];
+      s && "string" == typeof s && (s = {
+          type: s,
+          defaultValue: this.defaultValueForType(s)
+      });
+      const {
+          name: a = t,
+          type: n = "string",
+          defaultValue: i = ""
+      } = s;
+      let o;
+      if (r) o = r.get(a) ?? i;
+      else {
+          o = (this.getPluginParam(a) || {}).value ?? i;
+      }
+      return this.getParam({
+          value: o,
+          defaultValue: i,
+          type: n
+      })
+  }
+  static parseArrayParam({
+      value: t,
+      type: e
+  }) {
+      const r = this.parseArray(t);
+      if (!r || !r.length) return r;
+      const s = e.substr(0, e.length - 2),
+          a = [];
+      for (const t of r) {
+          const e = this.defaultValueForType(s);
+          a.push(this.getParam({
+              value: t,
+              type: s,
+              defaultValue: e
+          }));
+      }
+      return a
+  }
+  static getRegexMatch(t, e, r) {
+      const s = t.match(e);
+      if (s) return s[r]
+  }
+  static parseStructParam({
+      value: t,
+      defaultValue: e,
+      type: r
+  }) {
+      let s;
+      if (t) try {
+          s = JSON.parse(t);
+      } catch (e) {
+          console.error("SteamLink Engine failed to parse param structure: ", t), console.error(e);
+      }
+      s || (s = JSON.parse(e));
+      const a = this.getRegexMatch(r, /struct<(.*)>/i, 1);
+      if (!a) return console.error(`Unknown plugin param type: ${r}`), s;
+      const n = this.structs.get(a);
+      if (!n) return console.error(`Unknown param structure type: ${a}`), s;
+      for (const t in n) {
+          if (!n.hasOwnProperty(t)) continue;
+          let e = n[t];
+          "string" == typeof e && (e = {
+              type: e,
+              defaultValue: this.defaultValueForType(e)
+          }), s[t] = this.getParam({
+              value: s[t],
+              defaultValue: e.defaultValue,
+              type: e.type
+          });
+      }
+      return s
+  }
+  static parseList(t, e) {
+      let r = t;
+      r.startsWith("[") && (r = r.substr(1)), r.endsWith("]") && (r = r.substr(0, r.length - 1));
+      const s = r.split(",");
+      return e ? s.map((t => e(t))) : s
+  }
+  static parseArray(t, e) {
+      let r;
+      try {
+          r = JSON.parse(t);
+      } catch (t) {
+          return []
+      }
+      return r && r.length ? e ? r.map((t => e(t))) : r : []
+  }
+  static registerCommand(t, e, r) {
+      return "function" == typeof e ? PluginManager.registerCommand(this.getPluginFileName(), t, e) : PluginManager.registerCommand(this.getPluginFileName(), t, (t => {
+          const s = new Map;
+          for (const e in t) t.hasOwnProperty(e) && s.set(e, t[e]);
+          const a = this.loadParamMap(e, s);
+          return Object.assign(t, a), r(t)
+      }))
+  }
+}
+globalThis.SteamLinkPlugin = e;
+
+
+
+class SteamLink extends SteamLinkPlugin {
+  static register() {
+    this.initialized = false;
+    this.initialize('SteamLink');
+
+    super.register({});
+
+    if (typeof require !== 'function') {
+      return;
+    }
+
+    try {
+      this.greenworks = require('./greenworks');
+    } catch (e) {
+      this.greenworks = false;
+      console.error('Greenworks failed to load. Make sure you copied all files from the Steamworks SDK to the right folders;');
+      console.error(e);
+      return;
+    }
+
+    this.initialized = this.greenworks.initAPI();
+    if (!this.initialized) {
+      console.error('Greenworks failed to initialize.');
+      return;
+    }
+
+    this.steam = this.greenworks.getSteamId();
+  }
+
+  static get screenName() {
+    if (!this.greenworks) {
+      return 'Play Test';
+    }
+
+    return (this.steam && this.steam.screenName) || '';
+  }
+
+  static get uiLanguage() {
+    if (!this.greenworks) {
+      return 'english';
+    }
+
+    return this.greenworks.getCurrentUILanguage();
+  }
+
+  static get gameLanguage() {
+    if (!this.greenworks) {
+      return 'english';
+    }
+
+    return this.greenworks.getCurrentGameLanguage();
+  }
+
+  static get achievementCount() {
+    if (!this.greenworks) {
+      return 0;
+    }
+    return this.greenworks.getNumberOfAchievements();
+  }
+
+  static get running() {
+    return this.greenworks && this.greenworks.isSteamRunning();
+  }
+
+  static get overlayEnabled() {
+    return this.greenworks && this.greenworks.isGameOverlayEnabled();
+  }
+
+  static get dlcCount() {
+    if (!this.greenworks) {
+      return 0;
+    }
+
+    return this.greenworks.getDLCCount();
+  }
+
+  static get friendCount() {
+    if (!this.greenworks) {
+      return 0;
+    }
+
+    return this.greenworks.getFriendCount(this.greenworks.FriendFlags.Immediate);
+  }
+
+  static get cloudEnabled() {
+    if (!this.greenworks) {
+      return false;
+    }
+
+    return this.greenworks.isCloudEnabled();
+  }
+
+  static get userCloudEnabled() {
+    if (!this.greenworks) {
+      return false;
+    }
+
+    return this.greenworks.isCloudEnabledForUser();
+  }
+
+  static activateAchievement(achievementId) {
+    if (!achievementId) {
+      console.error('Achievement name not provided.');
+      return;
+    }
+
+    if (!this.greenworks) {
+      console.log(`Activate Achievement ${ achievementId }`);
+      return;
+    }
+
+    if (!this.running) {
+      return;
+    }
+
+    this.greenworks.activateAchievement(achievementId, () => {
+      console.log(`Achievement activated: ${ achievementId }`);
+    }, () => {
+      console.log(`Failed to activate achievement: ${ achievementId }`);
+    });
+  }
+
+  static getAchievement(achievementId) {
+    if (!achievementId) {
+      console.error('Achievement name not provided.');
+      return false;
+    }
+
+    if (!this.greenworks) {
+      return false;
+    }
+
+    if (!this.running) {
+      return;
+    }
+
+    return this.greenworks.getAchievement(achievementId, () => {
+      // #ToDo
+    }, () => {
+      console.log(`Failed to check achievement: ${ achievementId }`);
+    });
+  }
+
+  static clearAchievement(achievementId) {
+    if (!achievementId) {
+      console.error('Achievement name not provided.');
+      return;
+    }
+
+    if (!this.greenworks) {
+      console.log(`Clear achievement ${ achievementId }`);
+      return;
+    }
+
+    if (!this.running) {
+      return;
+    }
+
+    this.greenworks.clearAchievement(achievementId, () => {
+      console.log(`Successfully cleared achievement: ${ achievementId }`);
+    }, () => {
+      console.log(`Failed to clear achievement: ${ achievementId }`);
+    });
+  }
+
+  static activateGameOverlay(option) {
+    if (!this.running) {
+      return false;
+    }
+
+    this.greenworks.activateGameOverlay(option);
+  }
+
+  static activateGameOverlayToWebPage(url) {
+    if (!this.running) {
+      return false;
+    }
+
+    this.greenworks.activateGameOverlayToWebPage(url);
+  }
+
+  static isDLCInstalled(dlcAppId) {
+    if (!this.running) {
+      return false;
+    }
+
+    return this.greenworks.isDLCInstalled(dlcAppId);
+  }
+
+  static installDLC(dlcAppId) {
+    if (!this.running) {
+      return;
+    }
+
+    return this.greenworks.installDLC(dlcAppId);
+  }
+
+  static uninstallDLC(dlcAppId) {
+    if (!this.running) {
+      return;
+    }
+
+    return this.greenworks.uninstallDLC(dlcAppId);
+  }
+
+  static getStatInt(name) {
+    if (!this.running) {
+      return 0;
+    }
+
+    return this.greenworks.getStatInt(name);
+  }
+
+  static getStatFloat(name) {
+    if (!this.running) {
+      return 0;
+    }
+
+    return this.greenworks.getStatFloat(name);
+  }
+
+  static setStat(name, value) {
+    console.log('Change Stat', name, value);
+    if (!this.running) {
+      return 0;
+    }
+
+    this.greenworks.setStat(name, value);
+  }
+
+  static storeStats() {
+    console.log('Store Stats');
+    if (!this.running) {
+      return;
+    }
+
+    this.greenworks.storeStats(() => {
+      console.log('Store stats: success');
+    }, () => {
+      console.log('Store stats: failed');
+    });
+  }
+
+  static isSubscribedApp(appId) {
+    if (!this.running) {
+      return false;
+    }
+
+    return this.greenworks.isSubscribedApp(appId);
+  }
+}
+
+
+
+class SteamLinkPluginCommand {
+  static screenName({ variableId }) {
+    const result = SteamLink.screenName(); // Asume que SteamLink.screenName es una función que devuelve el nombre de la pantalla
+    $gameVariables.setValue(variableId, result);
+  }
+
+  static uiLanguage({ variableId }) {
+    const result = SteamLink.uiLanguage(); // Asume que SteamLink.uiLanguage es una función que devuelve el idioma de la UI
+    $gameVariables.setValue(variableId, result);
+  }
+
+  // Agrega aquí más métodos para los otros comandos
+}
+
+PluginManager.registerCommand('SteamLink', 'screenName', args => {
+  SteamLinkPluginCommand.screenName(args);
+});
+
+PluginManager.registerCommand('SteamLink', 'uiLanguage', args => {
+  SteamLinkPluginCommand.uiLanguage(args);
+});
+
+// Agrega aquí más registros para los otros comandos
+
+
+
+
+
+globalThis.SteamLink = SteamLink;
+SteamLink.register();
+})();
