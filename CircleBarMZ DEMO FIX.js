ad5196629c476223a2ebd08d24af19385fa6657a
Maxi Martínez <maxinm@live.com>
2023-07-11 15:41:13 -0300

    Update and rename CircleBarMZDEMOTOFIX.js to CircleBarMZ DEMO FIX.js

--- CircleBarMZ DEMO FIX.js
+++ CircleBarMZ DEMO FIX.js
@@ -1,11 +1,10 @@
 /*:
  * @target MZ
- * @plugindesc Create circular progress bars with lots of customization!
+ * @plugindesc Create circular progress bars with lots of customization! (DEMO VERSION!)
  * @url https://undermax.itch.io/
  * @author Maxii1996 | Undermax Games
  *
- *
- * @help
+ * 
  * 
     * @command showProgressBar
     * @text Show Progress Bar
@@ -56,24 +55,7 @@
     * @text Show All Progress Bars
     * @desc This enables the bar system. If you want to show a specific bar, use Show by ID.
     *
-    * @command hideProgressBarByID
-    * @text Hide Progress Bar By ID
-    * @desc Hides a progress bar with the specified ID.
-    *
-    * @arg id
-    * @type number
-    * @text ID
-    * @desc Unique ID of the progress bar.
-    *
-    * @command showProgressBarByID
-    * @text Show Progress Bar By ID
-    * @desc Shows a progress bar with the specified ID.
-    *
-    * @arg id
-    * @type number
-    * @text ID
-    * @desc Unique ID of the progress bar.
-    * 
+  
     * 
     */
 
@@ -95,10 +77,10 @@ class ProgressBar {
     this.maxValue = maxValue;
     this.posX = posX;
     this.posY = posY;
-    this.radius = 40;
-    this.lineWidth = 40;
-    this.color = ['#eb0000', '#eb0000', '#eb0000'];
-    this.backgroundColor = '#ffffff';
+    this.radius = 55;
+    this.lineWidth = 6;
+    this.color = ['#3335a3', '#4b00fa', '#c700d9'];
+    this.backgroundColor = '#24263044';
     this.sprite = sprite;
     this.sprite.bitmap = new Bitmap(Graphics.width, Graphics.height);
     this.progressBarValue = $gameVariables.value(this.actualValue);
@@ -106,20 +88,25 @@ class ProgressBar {
   }
 
   draw() {
-      let targetValue = $gameVariables.value(this.actualValue);
-      targetValue = Math.min(Math.max(targetValue, this.minValue), this.maxValue);
-      $gameVariables.setValue(this.actualValue, targetValue);
-    
-      let lerpSpeed = 0.035;
-      this.progressBarValue += (targetValue - this.progressBarValue) * lerpSpeed;
+    let targetValue = $gameVariables.value(this.actualValue);
+    targetValue = Math.min(Math.max(targetValue, this.minValue), this.maxValue);
+    $gameVariables.setValue(this.actualValue, targetValue);
 
-      let percentage = (this.progressBarValue - this.minValue) / (this.maxValue - this.minValue);
-      let angle = percentage * Math.PI * 2;
+    let percentage = (targetValue - this.minValue) / (this.maxValue - this.minValue);
+    let angle = percentage * Math.PI * 2;
 
-      this.sprite.bitmap.clear();
-      this.sprite.bitmap.drawCircle(this.posX, this.posY, this.radius, this.backgroundColor);
-      this.sprite.bitmap.drawArc(this.posX, this.posY, this.radius, 0, angle, this.color[0], this.lineWidth);
-  }
+    this.sprite.bitmap.clear();
+    this.sprite.bitmap.drawCircle(this.posX, this.posY, this.radius, this.backgroundColor);
+
+    let context = this.sprite.bitmap.context;
+    context.strokeStyle = this.color[0];
+    context.lineWidth = this.lineWidth;
+    context.beginPath();
+    context.arc(this.posX, this.posY, this.radius, 0, angle);
+    context.stroke();
+}
+
+
 
   hide() {
       this.targetOpacity = 0;
@@ -164,21 +151,7 @@ PluginManager.registerCommand('CircleBarMZDemo', 'showAllProgressBars', args => 
   });
 });
 
-PluginManager.registerCommand('CircleBarMZDemo', 'hideProgressBarByID', args => {
-  let id = 1;
-  let progressBar = $progressBars.find(progressBar => progressBar.id === id);
-  if (progressBar) {
-      progressBar.hide();
-  }
-});
 
-PluginManager.registerCommand('CircleBarMZDemo', 'showProgressBarByID', args => {
-  let id = 1;
-  let progressBar = $progressBars.find(progressBar => progressBar.id === id);
-  if (progressBar) {
-      progressBar.show();
-  }
-});
 
 const _Scene_Map_update = Scene_Map.prototype.update;
 Scene_Map.prototype.update = function() {
